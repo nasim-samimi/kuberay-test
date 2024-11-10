e6e2a1a8528cb15d1ab63d00548d3bc6c9c26dbc
20213285 <n.samimi.dehkordi@tue.nl>
2024-11-10 19:42:42 +0100

    ray test

--- mobilenet-imagenet/job.py
+++ mobilenet-imagenet/job.py
@@ -112,7 +112,8 @@ import time
 import pandas as pd
 
 # Apply real-time scheduling using chrt to the current process
-os.system('chrt -r 99 python -c "import os; os.getpid()"')
+os.system(f'chrt -r 99 -p {os.getpid()}')
+os.system(f'chrt -p {os.getpid()}')  # Verify the change
 
 # Ray initialization
 ray.init(address='auto')
@@ -151,7 +152,8 @@ def infer_image(image_path, model):
 @ray.remote
 def run_inference_on_directory(image_dir):
     # Apply chrt to this Ray worker process
-    os.system('chrt -r 99 python -c "import os; os.getpid()"')  # Apply real-time scheduling
+    os.system(f'chrt -r 99 -p {os.getpid()}')
+    os.system(f'chrt -p {os.getpid()}')  # Verify the change 
 
     model = load_model()
     results = {}
